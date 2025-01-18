9e70097e6452978aa35f366b9eb0e9a81c6732a1
jackiedeieso <deieso.j@northeastern.edu>
2025-01-17 20:16:49 -0500

    adding git link and netlify

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -9,9 +9,14 @@ export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
+      <h1>Jacqueline Deieso</h1>
+
+      <a href="https://github.com/jackiedeieso/kambaz" id="wd-github" target="_blank">My Repo</a><br/>
+      <a href="https://jackie-deieso-kambaz.netlify.app/#/Kambaz/Account/Signin" id="wd-netlify" target="_blank">My Repo</a><br/>
+
       <TOC />
       <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="/" element={<Navigate to="Lab1"/>} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2/*" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
