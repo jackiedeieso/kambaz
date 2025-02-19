3557ab00a8e3761b5805d75b61039f97d6642528
jackiedeieso <deieso.j@northeastern.edu>
2025-02-19 17:11:43 -0500

    commit

--- src/Labs/Lab 3/FindIndex.tsx
+++ src/Labs/Lab 3/FindIndex.tsx
@@ -1,5 +1,18 @@
-let numberArray1 = [1, 2, 4, 5, 6];
-let stringArray1 = ['string1', 'string3'];
+export default function findIndex() {
+    let numberArray1 = [1, 2, 4, 5, 6];
+    let stringArray1 = ['string1', 'string3'];
 
-const fourIndex = numberArray1.findIndex(a => a === 4);
-const string3Index = stringArray1.findIndex(a => a === 'string3');
+    const fourIndex = numberArray1.findIndex(a => a === 4);
+    const string3Index = stringArray1.findIndex(a => a === 'string3');
+
+    return (
+        <div>
+            <h3>Find Index Example</h3>
+            <p>Index of 4: {fourIndex}</p>
+            <p>Index of 'string3': {string3Index}</p>
+        </div>
+    );
+}
+
+
+
